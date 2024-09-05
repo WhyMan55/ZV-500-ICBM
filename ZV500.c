5bb8995a36cc1e631b2b8f2a7307a4fd10e4a029
Антон Ушаков <bruuuhv123456@gmail.com>
2024-09-05 18:55:37 +0300

    Add files via upload

---
+++ ZV500.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+void main()
+{
+	printf("your mother is a nice woman\n");
+	printf("  ______\n");
+	printf(" /      \\\n");
+	printf("/        \\\n");
+	printf("|--------|\n");
+	for (int i=0;i<8;i++)
+	{
+		printf("|        |\n");
+	}
+	printf("\\ DOBRO! /\n");
+	printf(" [------]\n");
+	printf(" | \\  / |\n");
+	printf(" |  \\/  |\n");
+	for (int i = 0;i < 6;i++)
+	{
+		printf(" |      |\n");
+	}
+	printf("/        \\\n");
+	printf("[========]\n");
+	printf("|  ___   |\n");
+	printf("|    /   |\n");
+	printf("|   /    |\n");
+	printf("|  /__   |\n");
+	printf("|        |\n");
+
+
+	printf("[/------\\]\n");
+	printf("  ****** \n");
+	printf("   ****\n");
+	printf("    **\n");
+	printf("    **\n");
+	
+}
